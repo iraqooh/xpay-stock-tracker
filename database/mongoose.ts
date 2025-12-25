7850c20c5d2e8198157a2e0be9653f37066fc2d2
Harry Iraku <iraqooh@gmail.com>
2025-12-25 17:32:08 +0300

    Implement databse setup

---
+++ database/mongoose.ts
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+import { cache } from 'react';
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+declare global {
+    var mongooseCache: {
+        conn: typeof mongoose | null,
+        promise: Promise<typeof mongoose> | null
+    }
+}
+
+let cached = global.mongooseCache;
+
+if(!cached) {
+    cached = global.mongooseCache = {
+        conn: null,
+        promise: null
+    }
+}
+
+export const connectToDatabase = async () => {
+    if(!MONGODB_URI) {
+        throw new Error('MONGODB_URI not set in the .env')
+    }
+
+    if(cached.promise) return cached.conn
+    
+    if(!cached.promise) {
+        cached.promise = mongoose.connect(
+            MONGODB_URI,
+            { bufferCommands: false }
+        )
+    }
+
+    try {
+        cached.conn = await cached.promise;
+    } catch (err) {
+        cached.promise = null;
+        throw err;
+    }
+
+    console.log(`Connected to database ${process.env.NODE_ENV} - ${MONGODB_URI}`)
+}
