48b16ae5c283d310cfa9530e0439dcfd866f8a4a
Harry Iraku <iraqooh@gmail.com>
2025-12-28 13:39:51 +0300

    Added search feature

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -1,30 +1,25 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import React from 'react'
-import NavItems from './NavItems'
-import UserDropdown from './UserDropdown'
+import Link from "next/link";
+import Image from "next/image";
+import NavItems from "./NavItems";
+import UserDropdown from "./UserDropdown";
+import {searchStocks} from "@/lib/actions/finnhub.actions";
 
-const Header = ({ user}: { user: User }) => {
-  return (
-    <header className='sticky top-0 header'>
-        <div className="container header-wrapper">
-            <Link href="/">
-                <Image 
-                    src="/assets/icons/xpay.svg" 
-                    alt="Xpay Logo"
-                    width={140}
-                    height={32}
-                />
-            </Link>
-            <nav className="hidden sm:block">
-                {/* Nav items */}
-                <NavItems />
-            </nav>
-            {/* User drop menu */}
-            <UserDropdown user={user} />
-        </div>
-    </header>
-  )
+const Header = async ({ user }: { user: User }) => {
+    const initialStocks = await searchStocks();
+
+    return (
+        <header className="sticky top-0 header">
+            <div className="container header-wrapper">
+                <Link href="/">
+                    <Image src="/assets/icons/logo.svg" alt="Signalist logo" width={140} height={32} className="h-8 w-auto cursor-pointer" />
+                </Link>
+                <nav className="hidden sm:block">
+                    <NavItems initialStocks={initialStocks} />
+                </nav>
+
+                <UserDropdown user={user} initialStocks={initialStocks} />
+            </div>
+        </header>
+    )
 }
-
 export default Header
